50c6940ecfe03c9e1a904250d2ea271239f9ccfa
CastleBomber <richie.ucsb@gmail.com>
2020-07-24 11:23:32 -0700

    finished second video

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // ref to rigigbody comp
+    public Rigidbody rb;
+
+    // mesh w/ physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
